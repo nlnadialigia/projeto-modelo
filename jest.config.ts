4f41f6a599a0d5bd9141efe5fe12f835dfa3ab10
Nadia Ligia <nlnadialigia@gmail.com>
2022-01-18 08:31:21 -0300

    chore: adicionar jest e configuraçoes

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  preset: 'ts-jest',
+  roots: [
+    '<rootDir>',
+  ],
+  testEnvironment: 'node',
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[tj]s?(x)',
+  ],
+  transform: { '.+\\.ts$': 'ts-jest' },
+  verbose: true,
+
+};
